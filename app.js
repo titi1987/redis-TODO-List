8f4ee1e9f17739321b6c4bd270d671cb63f17d39
titi1987 <lipot.pickermann@gmail.com>
2024-02-05 10:44:47 +0000

    first commit

--- app.js
+++ app.js
@@ -1,70 +1,94 @@
-//Declarations
+// Declarations
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const redis = require('redis');
+const { Pool } = require('pg'); // Import the Pool class from the pg module
 
-//Initialization of express
+// PostgreSQL Connection Setup
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'todo',
+  password: 'Reborn1987!',
+  port: 5432,
+});
+
+// Initialization of express
 var app = express();
 
-//Redis Client
+// Redis Client
 const client = redis.createClient();
-
 client.on('connect', () => {
   console.log('Connected to Redis...');
 });
 
-//View engine
-app.set('views'), path.join(__dirname, 'views');
+// View engine
+app.set('views', path.join(__dirname, 'views')); // Fix syntax here
 app.set('view engine', 'ejs');
 
-//Normalization of the elements used
+// Normalization of the elements used
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//This is our router
+// This is our router
 app.get('/', (req, res) => {
   var title = 'Redis TODO List';
-  var counter = 0;
-  client.LRANGE('todo', 0, -1, (err, reply) => {
-    if(err){
-      res.send(err);
+  
+  // Query tasks from PostgreSQL
+  pool.query('SELECT * FROM tasks ORDER BY id DESC', (err, result) => {
+    if (err) {
+      return res.send(err);
     }
+
     res.render('index', {
       title: title,
-      todo: reply,
-      counter: counter
+      todo: result.rows, // Updated to use data from PostgreSQL
+      counter: result.rowCount
     });
   });
 });
 
-//Add messages on redis
+// Add tasks to both Redis and PostgreSQL
 app.post('/todo/add', (req, res, next) => {
   var todo = req.body.todos;
+  
+  // Add to Redis
   client.RPUSH('todo', todo, (err, reply) => {
-    if(err){
-      res.send(err);
+    if (err) {
+      return res.send(err);
     }
-    res.redirect('/');
+
+    // Add to PostgreSQL
+    pool.query('INSERT INTO tasks (task) VALUES ($1)', [todo], (err, result) => {
+      if (err) {
+        return res.send(err);
+      }
+      res.redirect('/');
+    });
   });
 });
 
-//Delete messages on redis by index
+// Delete messages on redis by index (Modify as needed for PostgreSQL)
 app.post('/todo/delete', (req, res, next) => {
   var delTODO = req.body.todo;
   var deleted = '__DELETED__';
+  
   client.LRANGE('todo', 0, -1, (err, todo) => {
-    for(let i = 0; i < delTODO.length; i++){
+    for (let i = 0; i < delTODO.length; i++) {
       client.LSET('todo', delTODO[i], deleted);
     }
     client.LREM('todo', 0, deleted);
+    
+    // Additional logic needed here to delete from PostgreSQL if required
+    // This part is left as an exercise depending on how you want to handle deletions in PostgreSQL
+    
     res.redirect('/');
   });
 });
 
-//Port listen of the app
+// Port listen of the app
 app.listen(3000, () => {
   console.log('Server Started at port 3000...');
 });
